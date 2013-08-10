795ee9111c5f2a548e50a23e9a14b1caeda56930
mmcilroy <mmcilroy@hulk-01d.lan>
2013-08-11 00:02:27 +0100

    fix client test, fix message correctness

--- src/session.cpp
+++ src/session.cpp
@@ -1,6 +1,8 @@
 
 #include "hulk/fix/session.h"
 
+#include <cstdio>
+#include <ctime>
 #include <sstream>
 
 using namespace hulk::fix;
@@ -10,7 +12,27 @@ using namespace hulk::fix;
 #define CHECK_SUM 10
 #define MSG_TYPE 35
 #define MSG_SEQ_NUM 34
-#define DELIM '|'
+#define SENDING_TIME 52
+#define DELIM (char)1
+
+namespace
+{
+void set_utc_time( std::string& s, time_t* t )
+{
+    struct tm* utc = gmtime( t );
+
+    char buf[32];
+    sprintf( buf, "%04d%02d%02d-%02d:%02d:%02d",
+        utc->tm_year+1900,
+        utc->tm_mon+1,
+        utc->tm_mday,
+        utc->tm_hour,
+        utc->tm_min,
+        utc->tm_sec );
+
+    s = buf;
+}
+}
 
 session::session( const value& protocol, const fields& header, send_handler& sh, recv_handler& rh )
 : _protocol( protocol ),
@@ -22,12 +44,16 @@ session::session( const value& protocol, const fields& header, send_handler& sh,
 {
 }
 
-void
-session::send( const value& msg_type, const fields& body )
+void session::send( const value& msg_type, const fields& body )
 {
+    std::string send_time;
+    time_t t = time( 0 );
+    set_utc_time( send_time, &t );
+
     std::stringstream bodyStr;
     bodyStr << MSG_TYPE << "=" << msg_type << DELIM;
     bodyStr << MSG_SEQ_NUM << "=" << ++_seq_out << DELIM;
+    bodyStr << SENDING_TIME << "=" << send_time << DELIM;
 
     for( fields::const_iterator it = _header.begin(); it != _header.end(); it++ ) {
         bodyStr << it->first << "=" << it->second << DELIM;
@@ -43,13 +69,22 @@ session::send( const value& msg_type, const fields& body )
     msgStr << BEGIN_STRING << "=" << _protocol << DELIM;
     msgStr << BODY_LENGTH << "=" << length << DELIM;
     msgStr << bodyStr.str();
-    msgStr << CHECK_SUM << "=000" << DELIM;
+
+    int checksum = 0;
+    std::string s = msgStr.str();
+    for( int i=0; i<s.size(); i++ ) {
+        checksum += (int)s[i];
+    }
+
+    char buf[4];
+    sprintf( buf, "%03d", checksum % 256 );
+
+    msgStr << CHECK_SUM << "=" << buf << DELIM;
 
     _send_handler.send( msgStr.str() );
 }
 
-void
-session::recv( const fields& msg )
+void session::recv( const fields& msg )
 {
     _recv_handler.recv( msg );
 }
--- test/test-client.cpp
+++ test/test-client.cpp
@@ -1,73 +1,79 @@
 #include "hulk/core/tcp.h"
 #include "hulk/core/logger.h"
 
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <unistd.h>
+#include "hulk/fix/session.h"
 
+#include <sys/socket.h>
+
+using namespace hulk;
 using namespace hulk::core;
 
-void
-build_nos( std::string& str )
-{
-    std::stringstream body;
-    body << "8=FIX.4.2" << (char)1
-         << "9=0190" << (char)1
-         << "35=D" << (char)1
-         << "49=CGML" << (char)1
-         << "56=CHIX" << (char)1
-         << "52=20121011-11:10:11.048" << (char)1
-         << "34=457227" << (char)1
-         << "48=JE00B2R84W06" << (char)1
-         << "207=L" << (char)1
-         << "15=GBX" << (char)1
-         << "1=0004" << (char)1
-         << "11=920000019793712" << (char)1
-         << "38=65" << (char)1
-         << "40=2" << (char)1
-         << "44=731.5" << (char)1
-         << "60=20121011-11:10:11.015" << (char)1
-         << "54=1" << (char)1
-         << "47=P" << (char)1
-         << "59=3" << (char)1
-         << "9303=B" << (char)1
-         << "9479=X" << (char)1
-         << "10=025" << (char)1;
+log& l = logger::instance().get( "hulk.fix" );
 
-    str = body.str();
-}
-
-struct client_callback : public tcp_loop::callback
+struct client_send_handler : public fix::session::send_handler
 {
-    virtual void on_connect( int fd )
+    client_send_handler( int fd ) : _fd( fd ) {
+        ;
+    }
+
+    virtual void send( const std::string& msg )
     {
-        LOG_INFO( "client: " << fd << " connected" );
-
-        std::string nos;
-        build_nos( nos );
-
-        while( 1 ) {
-            write( fd, nos.c_str(), nos.size() );
-        }
+        LOG_DEBUG( l, "sending: " << msg );
+        ::send( _fd, msg.c_str(), msg.size(), 0 );
     }
 
-    virtual void on_disconnect( int fd ) {
-        LOG_INFO( "client: " << fd << " disconnected" );
-    }
+    int _fd;
+};
 
-    virtual void on_recv( int fd, const char* data, size_t len ) {
+struct client_recv_handler : public fix::session::recv_handler
+{
+    virtual void recv( const fix::fields& msg ) {
         ;
     }
 };
 
-int main()
+struct client_callback : public tcp::callback
 {
-    const char* host = "localhost";
-    const int port = 8000;
+    virtual void on_open( int fd ) {
+        ;
+    }
 
-    client_callback client_cb;
-    tcp_loop loop;
-    loop.tcp_client( "localhost", port, client_cb );
-    //loop.start();
+    virtual void on_close( int fd ) {
+        LOG_INFO( l, "on_close: " << fd );
+    }
+
+    virtual void on_recv( int fd, const char* data, size_t len ) {
+        LOG_INFO( l, "on_recv: " << fd << ", " << data );
+    }
+};
+
+int main( int argc, char** argv )
+{
+    const char* host = argv[1];
+    const int port = atoi( argv[2] );
+
+    LOG_INFO( l, "connecting to " << host << ":" << port );
+    int fd = tcp::connect( host, port );
+    LOG_INFO( l, "connected!" );
+
+    client_send_handler csh( fd );
+    client_recv_handler crh;
+    fix::fields header;
+    header[49] = "BANZAI";
+    header[56] = "EXEC";
+    fix::session session( "FIX.4.4", header, csh, crh );
+
+    LOG_INFO( l, "sending logon" );
+    fix::fields body;
+    body[98] = "0";
+    body[108] = "30";
+    session.send( "A", body );
+
+    LOG_INFO( l, "starting event loop" );
+    client_callback cb;
+    tcp::event_loop el( 1024, cb );
+    el.watch( fd, false );
+    while( 1 ) {
+        el.loop();
+    }
 }
